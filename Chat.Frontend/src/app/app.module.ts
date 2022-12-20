f83bcb487bb85ff554f92e817d1b88edb15adbbe
miaumere <40804624+miaumere@users.noreply.github.com>
2022-12-20 15:26:00 +0100

    added footer

--- Chat.Frontend/src/app/app.module.ts
+++ Chat.Frontend/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   bootstrap: [AppComponent],
 })
